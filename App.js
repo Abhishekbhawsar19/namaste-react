451742970c3404daf3e5b098b3152607e7a8dfad
Abhishek Bhawsar <=abhishekbhawsar71@gmail.com>
2026-08-20 20:36:43 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,31 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from React",
+);
+// heading is react element  when you console it, its object
+// {} where you give attributes  {id: 'heading'}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1></h1>
+    </div>
+</div>
+ReactElement(Object) ==> Html (Browser Understands)
+ */
+}
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: 1 }, "Iam h1 tag"),
+    React.createElement("h2", { key: 2 }, "Iam h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: 1 }, "Iam h1 tag"),
+    React.createElement("h2", { key: 2 }, "Iam h2 tag"),
+  ]),
+]);
+
+// Jsx 
+
+root.render(parent);
